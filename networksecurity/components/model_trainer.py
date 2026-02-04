d7163757888ec2c17bfa3800ffac3cf6286d9ae5
hardikjain0810 <hj791983@gmail.com>
2026-02-04 12:16:44 +0530

    Model trained and MLFlow completed

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import mlflow
 
 from networksecurity.exception.exception import NetworkSecurityException 
 from networksecurity.logging.logger import logging
@@ -36,6 +37,18 @@ class ModelTrainer:
             self.data_transformation_artifact=data_transformation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+    
+    def track_mlflow(self,best_model,classificationmetric):
+        with mlflow.start_run():
+            f1_score = classificationmetric.f1_score
+            precision_score = classificationmetric.precision_score
+            recall_score = classificationmetric.recall_score
+
+            mlflow.log_metric('f1_score',f1_score)
+            mlflow.log_metric('precision_score',precision_score)
+            mlflow.log_metric('recall_score',recall_score)
+            mlflow.sklearn.log_model(best_model,"model")
+
         
     def train_model(self,x_train,y_train,x_test,y_test):
         models = {
@@ -55,20 +68,20 @@ class ModelTrainer:
                 # 'criterion':['gini', 'entropy', 'log_loss'],
                 
                 # 'max_features':['sqrt','log2',None],
-                'n_estimators': [8,16,32,128,256]
+                'n_estimators': [8,16,32,256]
             },
             "Gradient Boosting":{
                 # 'loss':['log_loss', 'exponential'],
-                'learning_rate':[.1,.01,.05,.001],
+                'learning_rate':[.1,.01,.001],
                 'subsample':[0.6,0.7,0.75,0.85,0.9],
                 # 'criterion':['squared_error', 'friedman_mse'],
                 # 'max_features':['auto','sqrt','log2'],
-                'n_estimators': [8,16,32,64,128,256]
+                'n_estimators': [8,16,128,256]
             },
             "Logistic Regression":{},
             "AdaBoost":{
-                'learning_rate':[.1,.01,.001],
-                'n_estimators': [8,16,32,64,128,256]
+                'learning_rate':[.1,.001],
+                'n_estimators': [8,16,32,128,256]
             }
             
         }
@@ -88,12 +101,16 @@ class ModelTrainer:
 
         classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)  
 
-        # Track the MLflow
+        # Track the experiments with MLFlow
+        self.track_mlflow(best_model,classification_train_metric)
+
+
 
 
         y_test_pred=best_model.predict(x_test)
 
         classification_test_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
+        self.track_mlflow(best_model,classification_test_metric)
 
         preprocessor = load_object(self.data_transformation_artifact.transformed_object_file_path)
 
